37a63eafc2fbae013c8e27dbf8cb838a5f68b1dc
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-02-05 22:34:27 -0500

    added additional code to public folder

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const app = express();
 
 const fs = require('fs');
 const path = require('path');
+const { response } = require('express');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'))
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -94,6 +97,22 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
