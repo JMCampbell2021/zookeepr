35edc76f2b8529e65831cdb7850925d70700df7a
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-02-07 22:34:22 -0500

    updated

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,25 +26,25 @@ const handleAnimalFormSubmit = event => {
     personalityTraits.push(selectedTraits[i].value);
   }
   const animalObject = { name, species, diet, personalityTraits };
-  
-  fetch('/api/animals', {
-    method: 'Post',
+
+  fetch('api/animals', {
+    method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(animalObject)
   })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    alert('Error:' + response.statusText);
-  })
-  .then(postResponse => {
-    console.log(postResponse);
-    alert('Thank you for adding an animal!');
-  });
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
 };
 
 const handleZookeeperFormSubmit = event => {
@@ -77,5 +77,5 @@ const handleZookeeperFormSubmit = event => {
     });
 };
 
-$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
+$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
